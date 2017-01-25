ad474a105c135249cf7fb4dc39b8f55b1fd11027
Aasveen Kaur <aasveenkaur@gmail.com>
2017-01-24 21:07:20 -0800

    Project added

---
+++ LanscaperQuote/LanscaperQuote/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LanscaperQuote
+//
+//  Created by Aasveen Kaur on 1/24/17.
+//  Copyright © 2017 Aasveen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
