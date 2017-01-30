b46c6210cb27a41ea96d6b9fabad68a605c2aa35
anushaprabakaran <ANU@ANUSHAs-MBP.home>
2017-01-30 13:19:36 -0800

    LogIn page

---
+++ LanscaperQuote/LanscaperQuote/Constants.swift
@@ -0,0 +1,27 @@
+//
+//  Constants.swift
+//  LanscaperQuote
+//
+//  Created by ANUSHA on 1/30/17.
+//  Copyright © 2017 Aasveen. All rights reserved.
+//
+
+
+struct Constants {
+    
+    struct NotificationKeys {
+        static let SignedIn = "onSignInCompleted"
+    }
+    
+    struct Segues {
+        static let SignInToFp = "SignInToFP"
+        static let FpToSignIn = "FPToSignIn"
+    }
+    
+    struct MessageFields {
+        static let name = "name"
+        static let text = "text"
+        static let photoURL = "photoURL"
+        static let imageURL = "imageURL"
+    }
+}
